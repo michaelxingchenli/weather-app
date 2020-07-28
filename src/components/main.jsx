941244808cd092317e00716231bbf19960a937ae
Michael Li <sinfangli@gmail.com>
2020-07-28 14:25:50 -0700

    add wind direction

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,6 +4,10 @@
  import './../css/main.style.css'
 
  const Detail = ({temp, setUnit, unit, today, forcast}) => {
+  const windDirectionStyle = {
+    transform: 'rotate(' + today.wind_direction + 'deg)'
+  }
+
   return (
     <div className="container main">
       <div className="control-group" role="temp-control-group">
@@ -32,20 +36,23 @@
         <div className="row row-cols-1 row-cols-md-2 text-center">
           <div className="card mb-4 text-white">
             <h4>Wind status</h4>
-            <div className="wind">{Math.round(today.wind_speed)}<span class="unit">mph</span></div>
-            <div className="direction"><span className="material-icons circle">navigation</span>{today.wind_direction_compass}</div>
+            <div className="wind num">{Math.round(today.wind_speed)}<span className="unit">mph</span></div>
+            <div className="direction"><span className="material-icons circle" style={windDirectionStyle}>navigation</span>{today.wind_direction_compass}</div>
           </div>
           <div className="card mb-4 text-white">
             <h4>Humidity</h4>
-            <div className="humidity">{today.humidity} %</div>
+            <div className="humidity num">{today.humidity} <span className="unit">%</span></div>
+            <div className="progress">
+              <div className="progress-bar bg-warning" role="progressbar" style={{width: '66%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
           </div>
           <div className="card mb-4 text-white">
             <h4>Visibility</h4>
-            <div className="visibility">{Math.round(today.visibility)} miles</div>
+            <div className="visibility num">{Math.round(today.visibility)} <span className="unit">miles</span></div>
           </div>
           <div className="card mb-4 text-white">
             <h4>Air Pressure</h4>
-            <div className="air-pressure">{today.air_pressure} mb</div>
+            <div className="air-pressure num">{today.air_pressure} <span className="unit">mb</span></div>
           </div>
         </div>
       </div>
